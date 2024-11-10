4d79d3aaed4c6df3ed066d7b169dfdec79adef52
Petertan303 <63529504+Petertan303@users.noreply.github.com>
2024-11-10 13:30:11 +0800

    删除误上传的build文件夹

--- build/Desktop_Qt_6_8_0_MinGW_64_bit-Release/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_t4-2_FILE C:/Users/Peter_Tan/Documents/t4-2/build/Desktop_Qt_6_8_0_MinGW_64_bit-Release/t4-2.exe)
-set(__QT_DEPLOY_TARGET_t4-2_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_t4-2_RUNTIME_DLLS G:/Qt/6.8.0/mingw_64/bin/Qt6Widgets.dll;G:/Qt/6.8.0/mingw_64/bin/Qt6Network.dll;G:/Qt/6.8.0/mingw_64/bin/Qt6Gui.dll;G:/Qt/6.8.0/mingw_64/bin/Qt6Core.dll)
